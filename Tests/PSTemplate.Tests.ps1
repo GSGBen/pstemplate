e70fadf3efe2a933e4f44c82bf148919e344d80a
Ben Renninson <ben@goldensyrupgames.com>
2019-11-07 15:07:26 +1100

    test singleline strings

--- Tests/PSTemplate.Tests.ps1
+++ Tests/PSTemplate.Tests.ps1
@@ -24,14 +24,29 @@ line 1: Value1
 line 2: {{Token2}}
 "@
 
+$SingleLineStringTemplate = "Token 1: {{Token1}}; Token 2: {{Token2}}"
+
+$SingleLineStringFullyFilled = "Token 1: Value1; Token 2: Value2"
+
+$SingleLineStringLessFilled = "Token 1: Value1; Token 2: {{Token2}}"
+
+
 Describe "Fill-Template" {
 
-    It "replaces all {{<>}} tokens in a single multiline string with values in a hash table that contains all tokens as keys" {
+    It "replaces all {{<>}} tokens in a single multi-line string with values in a hash table that contains all tokens as keys" {
         Fill-Template -Template $MultilineStringTemplate -Variables $HashTableWithAllKeys | Should Be $MultilineStringFullyFilled
     }
 
-    It "replaces some {{<>}} tokens in a single multiline string with values in a hash table that contains some tokens as keys" {
+    It "replaces some {{<>}} tokens in a single multi-line string with values in a hash table that contains some tokens as keys" {
         Fill-Template -Template $MultilineStringTemplate -Variables $HashTableWithLessKeys | Should Be $MultilineStringLessFilled
+    }
+
+    It "replaces all {{<>}} tokens in a single single-line string with values in a hash table that contains all tokens as keys" {
+        Fill-Template -Template $SingleLineStringTemplate -Variables $HashTableWithAllKeys | Should Be $SingleLineStringFullyFilled
+    }
+
+    It "replaces some {{<>}} tokens in a single single-line string with values in a hash table that contains some tokens as keys" {
+        Fill-Template -Template $SingleLineStringTemplate -Variables $HashTableWithLessKeys | Should Be $SingleLineStringLessFilled
     }
 
 }
